26b157154b0dca5cbfab96bafb432bbe2ca308ac
androidmalin <malin.myemail@163.com>
2018-06-16 14:11:21 +0800

    init project

---
+++ app/src/main/java/com/malin/retrofit/MainActivity.java
@@ -0,0 +1,17 @@
+package com.malin.retrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
